419786358794bcc583f66d54066f4cc199355a1f
KatAlderman <kat@uxkat.co>
2018-05-30 12:28:23 -0400

    Login and Signup routing done

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -4,50 +4,48 @@ import { AuthService } from '../services/auth.service';
 import { ActivatedRoute } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
-
-  constructor(private myService: AuthService) {}
+  constructor(private myService: AuthService, private myRouter: Router) {}
 
   formInfo: any = {
-    username: '',
-    password: '',
-    email: '',
-    causes:''
+    username: "",
+    password: "",
+    email: "",
+    causes: ""
   };
 
-user: any;
-error: any;
-// title = 'app'; // ?
+  user: any;
+  error: any;
+  // title = 'app'; // ?
 
-signup() {
-  console.log('----');
-  console.log(this.formInfo);
-  this.myService.signup(this.formInfo)
-  .subscribe(
-    (user)  => {this.user = user;
-      console.log('Inside subscribe!');
+  signup() {
+    console.log("----");
+    console.log(this.formInfo);
+    this.myService.signup(this.formInfo).subscribe(user => {
+      this.user = user;
+      console.log("Inside subscribe!");
       console.log(this.formInfo);
-    },
-      (err) => this.error = err
-    );
-}
+      this.myRouter.navigate(["/login"]);
+    }, err => (this.error = err));
+  }
 
-ngOnInit() {
-  this.myService.isLoggedIn()
-  .toPromise()
-  .then(() => {
-    this.formInfo = this.myService.currentUser;
-  })
-  .catch(err => {
-    console.log(err);
-    // this.myRouter.navigate(['/login']);
-  });
-}
-
+  ngOnInit() {
+    this.myService
+      .isLoggedIn()
+      .toPromise()
+      .then(() => {
+        this.formInfo = this.myService.currentUser;
+      })
+      .catch(err => {
+        console.log(err);
+        //this.myRouter.navigate(["/login"]);
+      });
+  }
 }
